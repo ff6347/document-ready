33e3b887bc6c8dcdc16174225660a566489181ea
fabianmoronzirfas <fabian.moron.zirfas@gmail.com>
2017-09-24 14:55:38 +0200

    removed assert

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-'use strict'
+/* global document */
+/* eslint-disable no-undef*/
+'use strict';
 
-var assert = require('assert')
+var assert = require('assert');
 
-module.exports = ready
 
 function ready (callback) {
-  assert.notEqual(typeof document, 'undefined', 'document-ready only runs in the browser')
-  var state = document.readyState
+  if(typeof document === undefined) {
+    console.log('document-ready only runs in the browser');
+    return;
+  }
+  var state = document.readyState;
   if (state === 'complete' || state === 'interactive') {
-    return setTimeout(callback, 0)
+    return setTimeout(callback, 0);
   }
 
   document.addEventListener('DOMContentLoaded', function onLoad () {
-    callback()
-  })
+    callback();
+  });
 }
+
+module.exports = ready;
