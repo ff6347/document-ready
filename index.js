885a1e72cdf675e21cf6cb54872170106522a9eb
fabianmoronzirfas <fabian.moron.zirfas@gmail.com>
2017-09-29 13:38:26 +0200

    minor

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 /* global document */
-/* eslint-disable no-undef*/
+/* eslint-disable no-undef */
 'use strict';
 
-var assert = require('assert');
-
-
-function ready (callback) {
-  if(typeof document === undefined) {
+function ready(callback) {
+  if (typeof document === undefined) {
     console.log('document-ready only runs in the browser');
     return;
   }
-  var state = document.readyState;
+  const state = document.readyState;
   if (state === 'complete' || state === 'interactive') {
     return setTimeout(callback, 0);
   }
 
-  document.addEventListener('DOMContentLoaded', function onLoad () {
+  document.addEventListener('DOMContentLoaded', () => {
     callback();
   });
 }
